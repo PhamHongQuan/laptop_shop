1243e2f629bfca09325c2b2c6afa45f6f36825ea
Hong Quan <qp57744@gmail.com>
2026-06-28 00:08:15 +0700

    Revert "dev: create database"
    This reverts commit a6aa58479450fee925605abe4cc7241653c04b8e.

--- src/database/migrations/0001_01_01_000000_create_users_table.php
+++ src/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,16 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100);
+            $table->string('name');
             $table->string('email')->unique();
-            $table->string('phone', 20)->nullable();
-            $table->string('avatar')->nullable();
-            $table->tinyInteger('role')->default(0)->index();
-            $table->tinyInteger('status')->default(1)->index();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->softDeletes();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
